d3722a27eddaae58f11d41e1833ebc9a5a631cde
ChrisDev <Christianpeya@hotmail.com>
2024-10-18 20:21:36 -0600

    Actualizando el proyecto

---
+++ src/main/java/com/example/gestion_productos/HelloController.java
@@ -0,0 +1,35 @@
+package com.example.gestion_productos;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+
+    @FXML
+    private Button AgregarProducto;
+
+    @FXML
+    private TextField CantidadProducto;
+
+    @FXML
+    private TextField NombreProducto;
+
+    @FXML
+    private TextField categoriaProducto;
+
+    @FXML
+    private TextField precioProducto;
+
+    @FXML
+    void agregarProducto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eliminarProducto(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.gestion_productos {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.gestion_productos to javafx.fxml;
+    exports com.example.gestion_productos;
+}
