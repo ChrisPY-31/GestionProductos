29c64f71d5e7a03942b11fa92f2b2488b85a68dc
marterann24 <mariteran920@gmail.com>
2024-10-20 22:45:07 -0600

    Se agrego diseño

---
+++ src/main/java/Modelo/AgregarProductos.java
@@ -0,0 +1,42 @@
+package com.example.gestion_productos;
+
+public class AgregarProductos {
+    public String nombreProducto;
+    public String categoria;
+    public double precio;
+    public int cantidad;
+
+    public AgregarProductos(String nombre, String categoria, double precio, int cantidad) {
+        this.nombreProducto = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    public String getNombre(){
+        return nombreProducto;
+    }
+    public void setnombreProducto(String nombreProducto){
+        this.nombreProducto = nombreProducto;
+    }
+    public String getCategoria(){
+        return categoria;
+    }
+    public void setcategoria(String categoria){
+        this.categoria = categoria;
+    }
+    public double getPrecio(){
+        return precio;
+    }
+    public void setPrecio(double precio){
+        this.precio = precio;
+    }
+    public int getCantidad(){
+        return cantidad;
+    }
+    public void setCantidad(int cantidad){
+        this.cantidad = cantidad;
+    }
+
+
+}
