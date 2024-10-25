ba07819dd0dae415c65cf2192280a967af2345f0
ChrisDev <Christianpeya@hotmail.com>
2024-10-25 12:44:28 -0600

    Navegabilidad de Usuarios Concluido

--- src/main/java/Controllers/UsuarioController.java
+++ src/main/java/Controllers/UsuarioController.java
@@ -2,8 +2,22 @@ package Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class UsuarioController {
+
+
+    @FXML
+    private Text txtProductosTitulo;
+
 
     @FXML
     void btnEditarPrecio(ActionEvent event) {
@@ -11,13 +25,65 @@ public class UsuarioController {
     }
 
     @FXML
-    void btnVistaComprar(ActionEvent event) {
+    void btnVistaComprar() {
+        //Navegabilidad de la vista Comprar comcluida
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vistas/CompraProductos.fxml"));
+            Parent root = loader.load();
+            CompraController controller = loader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
 
+            stage.setOnCloseRequest(e -> controller.btnInicio());
+            stage.setOnCloseRequest(e -> controller.closeWindowsCompra());
+
+            Stage myStage = (Stage) this.txtProductosTitulo.getScene().getWindow();
+            myStage.close();
+        } catch (IOException e) {
+            Logger.getLogger(UsuarioController.class.getName()).log(Level.SEVERE, null, e);
+
+        }
     }
 
     @FXML
     void btnVistaVender(ActionEvent event) {
+        //Navegabilidad de la vista VenderCompletada
 
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vistas/VentaProducto.fxml"));
+            Parent root = loader.load();
+            VentaProductoController controller = loader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+
+            stage.setOnCloseRequest(e -> controller.btnVistaInicio());
+            stage.setOnCloseRequest(e -> controller.closeWindowsVender());
+
+            Stage myStage = (Stage) this.txtProductosTitulo.getScene().getWindow();
+            myStage.close();
+        } catch (IOException e) {
+            Logger.getLogger(UsuarioController.class.getName()).log(Level.SEVERE, null, e);
+
+        }
+    }
+
+
+    @FXML
+    void tblVerProductos(ActionEvent event) {
+
+    }
+    public void closeWindowsUser() {
+        try {
+            Stage myStage = (Stage) this.txtProductosTitulo.getScene().getWindow();
+            myStage.close();
+
+        } catch (Exception e) {
+            Logger.getLogger(AgregarProductosController.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
 
 }
--- src/main/java/Controllers/VentaProductoController.java
+++ src/main/java/Controllers/VentaProductoController.java
@@ -1,4 +1,78 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class VentaProductoController {
+
+    @FXML
+    private Text TituloProductos;
+
+    @FXML
+    void btnVenderProductos() {
+        //vista vender
+
+    }
+
+    @FXML
+    void btnVistaComprar(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vistas/CompraProductos.fxml"));
+            Parent root = loader.load();
+            CompraController controller = loader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+
+            stage.setOnCloseRequest(e -> controller.btnVender());
+
+            Stage myStage = (Stage) this.TituloProductos.getScene().getWindow();
+            myStage.close();
+        } catch (IOException e) {
+            Logger.getLogger(VentaProductoController.class.getName()).log(Level.SEVERE, null, e);
+
+        }
+    }
+
+    @FXML
+    void btnVistaInicio() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Vistas/Usuario.fxml"));
+            Parent root = loader.load();
+            UsuarioController controller = loader.getController();
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+
+            stage.setOnCloseRequest(e -> controller.closeWindowsUser());
+
+            Stage myStage = (Stage) this.TituloProductos.getScene().getWindow();
+            myStage.close();
+        } catch (IOException e) {
+            Logger.getLogger(VentaProductoController.class.getName()).log(Level.SEVERE, null, e);
+
+        }
+    }
+
+    public void closeWindowsVender() {
+        try {
+            Stage myStage = (Stage) this.TituloProductos.getScene().getWindow();
+            myStage.close();
+
+        } catch (Exception e) {
+            Logger.getLogger(VentaProductoController.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
 }
