9be64214f7ede96486d634d26ab5cf9f0c075ad8
marterann24 <mariteran920@gmail.com>
2024-11-18 01:19:02 -0600

    Cambios vistas Administrador

--- src/main/java/Controllers/AgregarProductosController.java
+++ src/main/java/Controllers/AgregarProductosController.java
@@ -170,21 +170,21 @@ public class AgregarProductosController implements Initializable {
         CategoriaList.setItems(list);
 
         txtPrecio.focusedProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue) { // Cuando pierde el foco
-                lblMensajePrecio.setVisible(false); // Ocultar el mensaje
+            if (!newValue) {
+                lblMensajePrecio.setVisible(false); // Oculta el mensaje
             }
         });
 
         txtPrecio.textProperty().addListener((observable, oldValue, newValue) -> {
             if (!newValue.isEmpty()) {
-                lblMensajePrecio.setVisible(true); // Mostrar mensaje mientras escribe
+                lblMensajePrecio.setVisible(true); // Se muestra mensaje mientras escribe
             } else {
-                lblMensajePrecio.setVisible(false); // Ocultar mensaje si está vacío
+                lblMensajePrecio.setVisible(false); // oculta mensaje
             }
         });
 
         txtCantidad.focusedProperty().addListener((observable, oldValue, newValue) -> {
-            if (!newValue) { // Cuando pierde el foco
+            if (!newValue) {
                 lblMensajeCantidad.setVisible(false); // Ocultar el mensaje
             }
         });
@@ -193,14 +193,14 @@ public class AgregarProductosController implements Initializable {
             if (!newValue.isEmpty()) {
                 lblMensajeCantidad.setVisible(true); // Mostrar mensaje mientras escribe
             } else {
-                lblMensajeCantidad.setVisible(false); // Ocultar mensaje si está vacío
+                lblMensajeCantidad.setVisible(false); //ocultar mensaje cuando seleccione una opcion
             }
         });
 
         // Listener para detectar selección de una opción
         CategoriaList.valueProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue != null && !newValue.isEmpty()) {
-                lblSeleccionaCategoria.setVisible(false); // Oculta el Label
+                lblSeleccionaCategoria.setVisible(false); // Se oculta el Label
             } else {
                 lblSeleccionaCategoria.setVisible(true); // Muestra el Label si no hay selección
             }
